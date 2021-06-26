0f9828b363d79acc02b826ce488333ce326d5796
SaurabhS78 <saurabhsangam78@gmail.com>
2021-06-27 03:05:54 +0530

    refactor email routers

--- server/src/index.js
+++ server/src/index.js
@@ -5,8 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const userRouter = require("./routers/userRouters");
+const emailRouter = require("./routers/emailRouters");
 
 app.use(express.json());
-app.use(userRouter);
+app.use(userRouter, emailRouter);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

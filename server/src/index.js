82609d9ed305c11c9298e7e456927b6bd36191a3
SaurabhS78 <saurabhsangam78@gmail.com>
2021-06-27 01:17:18 +0530

    enabled cors for all origins

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 require("./db/mongoose");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const userRouter = require("./routers/userRouters");
 
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 
